4cc5a2fa49dd7c9820c0db3ac6271ab4ffe71345
Merekeyev Dias <dikxarper.programming@gmail.com>
2024-04-20 14:32:22 +0500

    added header

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser')
 const path = require('path')
 const expressLayouts = require('express-ejs-layouts')
 require("dotenv").config({path: "config/.env"})
-// const index = require('./routes/index')
+const index = require('./routes/index')
 // const adminRoute = require('./routes/adminRoute')
 
 app.set('views', path.join(__dirname, 'views'))
